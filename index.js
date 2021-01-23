d76143cb4cce1569be13c242cde580e3c7c6e467
birukyemane <bir_yem@yahoo.com>
2021-01-24 00:22:43 +0200

    - add Cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const observationRouter = require('./routes/observationRouter');
 
 const url = 'mongodb+srv://test-user:test-user-123@cluster0.xceuv.mongodb.net/weather-app?retryWrites=true&w=majority';
@@ -9,7 +10,7 @@ const hostname = 'localhost';
 const port = 3001;
 
 const app = express();
-
+app.use(cors());
 app.use('/observations', observationRouter);
 
 const server = http.createServer(app);
