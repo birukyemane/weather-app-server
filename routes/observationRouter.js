54cfeccce17837e741ea6481164cb5fb5b03ce50
birukyemane <bir_yem@yahoo.com>
2021-01-23 15:58:14 +0200

    - define router

---
+++ routes/observationRouter.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Observations = require('../models/observations');
+
+const observationRouter = express.Router();
+
+observationRouter.use(bodyParser.json());
+
+observationRouter.route('/')
+.get((req,res,next) => {
+    Observations.find({})
+    .then((observations) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(observations);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    Observations.create(req.body)
+    .then((observation) => {
+        console.log('Observation Created ', observation);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(observation);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put((req, res, next) => {
+    Observations.findOne({point: req.body.point}, function (err, doc) {
+        if (err) next(err);
+        doc.temperatures.push({value:req.body.value})
+        doc.save()
+            .then((observation) => {
+                console.log('Observation Updated ', observation);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(observation);
+            })
+            .catch((err) => next(err));
+    })
+})
+.delete((req, res, next) => {
+    Observations.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));    
+});
+
+module.exports = observationRouter;
